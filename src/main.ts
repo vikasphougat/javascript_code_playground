ba3de9982fb622eef6a8f940c4a53b4bbef07907
vikas phougat <vikas.phougat@kalolytic.com>
2023-05-10 14:21:01 +0530

    recent project

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,102 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
 import { setupCounter } from './counter'
+import firebase, { initializeApp }  from 'firebase/app';
+import  {getFirestore} from 'firebase/firestore';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+interface firebasetype {
+  apiKey: String
+  authDomain: String
+  projectId: String
+  storageBucket: String
+  messagingSenderId: String
+  appId: String
+  measurementId: String
+}
+const firebaseConfig: firebasetype = {
+  apiKey: "AIzaSyAADxEYn9B9PnPDYBTwVuYTuBrhiPEw_Jg",
+  authDomain: "fir-rtctesting-5d9ed.firebaseapp.com",
+  projectId: "fir-rtctesting-5d9ed",
+  storageBucket: "fir-rtctesting-5d9ed.appspot.com",
+  messagingSenderId: "47508261309",
+  appId: "1:47508261309:web:adae39a08854f7b82a2ad4",
+  measurementId: "G-5VHL01D1MJ"
+};
+const app = initializeApp(firebaseConfig);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// if(!firebase.apps.length){
+//   firebase.initializeApp(firebaseConfig);
+// }
+
+const firestore :any  = getFirestore(app)
+
+
+
+// globel state 
+const servers:any = {
+  iceServers: [
+    {
+      urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
+    },
+  ],
+  iceCandidatePoolSize: 10,
+};
+let pc:any = new RTCPeerConnection();
+let localStream:any = null;
+let remoteStream:any = null;
+
+// HTML elements
+const webcamButton :HTMLElement | null = document.getElementById('webcamButton');
+const webcamVideo = document.getElementById('webcamVideo');
+const callButton = document.getElementById('callButton');
+const callInput = document.getElementById('callInput');
+const answerButton = document.getElementById('answerButton');
+const remoteVideo = document.getElementById('remoteVideo');
+const hangupButton = document.getElementById('hangupButton');
+
+
+
+
+
+// webcam 
+webcamButton.onclick = async () => {
+  localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+
+  // Push tracks from local stream to peer connection
+  localStream.getTracks().forEach((track:any) => {
+      pc.addTrack(track, localStream);
+  });
+
+  // Pull tracks from remote stream, add to video stream
+  pc.ontrack = (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+  };
+
+  // Show stream in HTML video
+  webcamVideo.srcObject = localStream;
+  remoteVideo.srcObject = remoteStream;
+
+
+
+  callButton.disabled = false;
+  answerButton.disabled = false;
+  webcamButton.disabled = true;
+}
+
+
+// 2. Create an offer
+callButton.onclick = async () => {
+  // Reference Firestore collections for signaling
+  const callDoc = firestore.collection('calls').doc();
+  const offerCandidates = callDoc.collection('offerCandidates');
+  const answerCandidates = callDoc.collection('answerCandidates');
+
+  callInput.value = callDoc.id;
+
+  // Get candidates for caller, save to db
+  pc.onicecandidate = (event) => {
+    event.candidate && offerCandidates.add(event.candidate.toJSON());
+  };
+}
---
+++ src/pages/webrtc/webrtc.ts
@@ -0,0 +1,15 @@
+import webRtcStyle  from './webrtc.module.css';
+
+
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+   webRtc working 
+  </div>
+`
+
+
+
+
+// export default webrtcfile;
